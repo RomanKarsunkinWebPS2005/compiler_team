b50427c7b4ddcf202cef64bf8409573830007c39
APekunova <159965116+APekunova@users.noreply.github.com>
2025-10-02 12:09:09 +0300

    Update Reverse.py

--- docs/competitors/Python/Reverse/Reverse.py
+++ docs/competitors/Python/Reverse/Reverse.py
@@ -1,8 +1,9 @@
-﻿//Reverse — читает строку и печатает её в перевёрнутом виде
+﻿#Reverse — читает строку и печатает её в перевёрнутом виде
 
 input_str = input("Enter input string: ")
 reversed_str = input_str[::-1]
 
 print("Input string:", input_str)
 print("Reversed string:", reversed_str)
+
 
